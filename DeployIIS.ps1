e305fbf77268054eab2b71d302cb67968ab6c035
oshalygin <oshalygin@gmail.com>
2015-10-18 21:04:51 -0700

    refactored to use global

--- DeployIIS.ps1
+++ DeployIIS.ps1
@@ -1,57 +1,63 @@
+
+$siteName = "TechieJS"
 $session = New-PSSession
 
-Invoke-Command -Session $session -ScriptBlock {
+$deploymentScript = {
 
-Import-Module WebAdministration
+    Param($siteName)
 
-Set-Location IIS:
+    Import-Module WebAdministration
 
-$siteName = "SolutionPub"
-$appPoolName = $siteName
-$protocol = "http"
-$port = "9000"
-$projectPath = "C:\dev\SolutionPub"
-$binding = "*:" + $port + ":"
+    Set-Location IIS:
 
-Write-Host -ForegroundColor DarkGreen "Deploying $siteName to local IIS"
+    #$siteName = "SolutionPub"
+    $appPoolName = $siteName
+    $protocol = "http"
+    $port = "9000"
+    $projectPath = "C:\dev\TechieJS\TN6\TN.Web\"
+    $binding = "*:" + $port + ":"
 
-$appPoolExists = Test-Path IIS:\AppPools\$appPoolName
-$siteExists = Test-Path IIS:\Sites\$siteName
+    Write-Host -ForegroundColor DarkGreen "Deploying $siteName to local IIS"
+
+    $appPoolExists = Test-Path IIS:\AppPools\$appPoolName
+    $siteExists = Test-Path IIS:\Sites\$siteName
 
 
-If($appPoolExists -eq $false) {
-    Write-Host -ForegroundColor DarkGreen "Creating ApplicationPool: $appPoolName..."
-    New-Item IIS:\AppPools\$appPoolName -ea Stop
-    Write-Host -ForegroundColor DarkGreen "Created ApplicationPool: $appPoolName..."
+    If($appPoolExists -eq $false) {
+        Write-Host -ForegroundColor DarkGreen "Creating ApplicationPool: $appPoolName..."
+        New-Item IIS:\AppPools\$appPoolName -ea Stop
+        Write-Host -ForegroundColor DarkGreen "Created ApplicationPool: $appPoolName..."
+    }
+    Else {
+        Write-Host -ForegroundColor DarkRed "ApplicationPool: $appPoolName already exists, Skipping..."
+    }
+
+
+    If($siteExists -eq $false) {
+        Write-Host -ForegroundColor DarkGreen "Creating WebSite: $siteName..."
+        New-Item IIS:\Sites\$siteName -bindings @{protocol=$protocol;bindingInformation=$binding} -physicalPath $projectPath -applicationPool $appPoolName -ea SilentlyContinue
+        Write-Host -ForegroundColor DarkGreen "Created WebSite: $siteName..."
+    }
+    Else {
+        Write-Host -ForegroundColor DarkRed "WebSite: $siteName already exists, Skipping..."
+        Write-Host -ForegroundColor DarkRed "Exiting Deployment Script..."
+        exit
+    }
+
+    Write-Host -ForegroundColor DarkGreen "Restarting IIS, W3SVC and WAS(Windows Activation Services)"
+
+        IISRESET /Restart
+        Restart-Service W3SVC,WAS -Force
+
+        #Disabling Anonymous Authentication
+        Write-Host -ForegroundColor DarkGreen "Disabling Anonymous Authentication"
+        Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/anonymousAuthentication" -Name Enabled -Value False
+        Write-Host -ForegroundColor DarkGreen "Disabled Anonymous Authentication"
+
+        #Enabling Windows Authentication
+        Write-Host -ForegroundColor DarkGreen "Enabling Windows Authentication"
+        Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/windowsAuthentication" -Name Enabled -Value True
+        Write-Host -ForegroundColor DarkGreen "Enabled Windows Authentication"
 }
-Else {
-    Write-Host -ForegroundColor DarkRed "ApplicationPool: $appPoolName already exists, Skipping..."
-}
 
-
-If($siteExists -eq $false) {
-    Write-Host -ForegroundColor DarkGreen "Creating WebSite: $siteName..."
-    New-Item IIS:\Sites\$siteName -bindings @{protocol=$protocol;bindingInformation=$binding} -physicalPath $projectPath -applicationPool $appPoolName -ea SilentlyContinue
-    Write-Host -ForegroundColor DarkGreen "Created WebSite: $siteName..."
-}
-Else {
-    Write-Host -ForegroundColor DarkRed "WebSite: $siteName already exists, Skipping..."
-    Write-Host -ForegroundColor DarkRed "Exiting Deployment Script..."
-    exit
-}
-
-Write-Host -ForegroundColor DarkGreen "Restarting IIS, W3SVC and WAS(Windows Activation Services)"
-
-    IISRESET /Restart
-    Restart-Service W3SVC,WAS -Force
-
-    #Disabling Anonymous Authentication
-    Write-Host -ForegroundColor DarkGreen "Disabling Anonymous Authentication"
-    Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/anonymousAuthentication" -Name Enabled -Value False
-    Write-Host -ForegroundColor DarkGreen "Disabled Anonymous Authentication"
-
-    #Enabling Windows Authentication
-    Write-Host -ForegroundColor DarkGreen "Enabling Windows Authentication"
-    Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/windowsAuthentication" -Name Enabled -Value True
-    Write-Host -ForegroundColor DarkGreen "Enabled Windows Authentication"
-}
+Invoke-Command -Session $session -ScriptBlock $deploymentScript -ArgumentList $siteName
